f40c21eed0601a25aebe3249c62f8e7b20e9ee5b
mbedingfield <mbedingfield@dcrpos.com>
2019-11-30 16:01:30 -0600

    converts example app to functional component instead of a class component

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import {
   ColorHeadingOne,
@@ -6,30 +6,25 @@ import {
   ModalDatePicker
 } from "c5-react-library";
 
-export default class App extends Component {
-  state = {
-    open: false,
-    time: new Date(),
-    isOpen: false
+const App = () => {
+  const [time, setTime] = useState(new Date());
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenDatePicker = () => {
+    setIsOpen(!isOpen);
   };
 
-  toggleState = () => {
-    this.setState(prevState => ({ open: !prevState.open }));
+  const handleDateSelect = time => {
+    setTime(time);
+    setIsOpen(false);
   };
 
-  handleOpenDatePicker = () => {
-    this.setState({ isOpen: true });
-  };
-  handleDateSelect = time => {
-    this.setState({ time, isOpen: false });
-    console.log(time);
+  const handleCloseDatePicker = () => {
+    setIsOpen(false);
   };
 
-  handleCloseDatePicker = () => {
-    this.setState({ isOpen: false });
-  };
-
-  formatDate = date => {
+  const formatDate = date => {
     var testDate = new Date(date),
       month = testDate.getMonth() + 1,
       day = testDate.getDate(),
@@ -38,33 +33,32 @@ export default class App extends Component {
     return month + "/" + day + "/" + year;
   };
 
-  render() {
-    return (
-      <div style={{ width: 800, margin: "15px auto" }}>
-        <ColorHeadingOne text="Colored heading one is working" color={"red"} />
+  return (
+    <div style={{ width: 800, margin: "15px auto" }}>
+      <ColorHeadingOne text="Colored heading one is working" color={"red"} />
 
-        {/* Our other component */}
-        <ColorHeadingTwo text="Another Heading" color={"blue"} />
+      {/* Our other component */}
+      <ColorHeadingTwo text="Another Heading" color={"blue"} />
 
-        {/* This is our Modal Component */}
-        <button onClick={this.toggleState}>Open Modal</button>
-        <div style={{ marginTop: "30px" }}>
-          <button onClick={this.handleOpenDatePicker}>Select Date</button>
-          <ModalDatePicker
-            value={this.state.time}
-            isOpen={this.state.isOpen}
-            onSelect={this.handleDateSelect}
-            onCancel={this.handleCloseDatePicker}
-          />
-        </div>
-        <div>
-          <input
-            style={{ marginTop: "20px" }}
-            type="text"
-            value={this.formatDate(this.state.time)}
-          />
-        </div>
+      {/* This is our Modal Component */}
+      <div style={{ marginTop: "30px" }}>
+        <button onClick={handleOpenDatePicker}>Select Date</button>
+        <ModalDatePicker
+          value={time}
+          isOpen={isOpen}
+          onSelect={handleDateSelect}
+          onCancel={handleCloseDatePicker}
+        />
       </div>
-    );
-  }
-}
+      <div>
+        <input
+          style={{ marginTop: "20px" }}
+          type="text"
+          value={formatDate(time)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
